c474890d6e7bba57f5f78f226e5b2cc7572368cd
Philipp Eichhorn <philipp.eichhorn@bitdroid.de>
2015-02-16 00:55:26 +0100

    ui: use DI In MainActivity

--- app/src/main/java/org/faudroids/tripweather/ui/MainActivity.java
+++ app/src/main/java/org/faudroids/tripweather/ui/MainActivity.java
@@ -1,49 +1,26 @@
 package org.faudroids.tripweather.ui;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.AutoCompleteTextView;
 
 import org.faudroids.tripweather.R;
 
-public class MainActivity extends ActionBarActivity {
+import roboguice.activity.RoboActivity;
+import roboguice.inject.ContentView;
+import roboguice.inject.InjectView;
+
+
+@ContentView(R.layout.activity_main)
+public class MainActivity extends RoboActivity {
+
+	@InjectView(R.id.autocomplete_from) AutoCompleteTextView autoCompleteFrom;
+	@InjectView(R.id.autocomplete_to) AutoCompleteTextView autoCompleteTo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        AutoCompleteTextView autoCompleteFrom = (AutoCompleteTextView) findViewById(R.id
-                .autocomplete_from);
-        autoCompleteFrom.setAdapter(new PlacesAutoCompleteAdapter(this,
-                R.layout.autocomplete_item));
-        AutoCompleteTextView autoCompleteTo = (AutoCompleteTextView) findViewById(R.id
-                .autocomplete_to);
+        autoCompleteFrom.setAdapter(new PlacesAutoCompleteAdapter(this, R.layout.autocomplete_item));
         autoCompleteTo.setAdapter(new PlacesAutoCompleteAdapter(this, R.layout.autocomplete_item));
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
