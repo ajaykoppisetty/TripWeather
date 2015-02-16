e649298b7f4bdd5ac6c43a0b5e88787f7513f776
Philipp Eichhorn <philipp.eichhorn@bitdroid.de>
2015-02-16 01:03:33 +0100

    ui: added short WeaterService example to MainActivity

--- app/src/main/java/org/faudroids/tripweather/ui/MainActivity.java
+++ app/src/main/java/org/faudroids/tripweather/ui/MainActivity.java
@@ -2,9 +2,18 @@ package org.faudroids.tripweather.ui;
 
 import android.os.Bundle;
 import android.widget.AutoCompleteTextView;
+import android.widget.Toast;
+
+import com.fasterxml.jackson.databind.JsonNode;
 
 import org.faudroids.tripweather.R;
+import org.faudroids.tripweather.weather.WeatherService;
 
+import javax.inject.Inject;
+
+import retrofit.Callback;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
 import roboguice.activity.RoboActivity;
 import roboguice.inject.ContentView;
 import roboguice.inject.InjectView;
@@ -15,12 +24,33 @@ public class MainActivity extends RoboActivity {
 
 	@InjectView(R.id.autocomplete_from) AutoCompleteTextView autoCompleteFrom;
 	@InjectView(R.id.autocomplete_to) AutoCompleteTextView autoCompleteTo;
+	@Inject WeatherService weatherService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         autoCompleteFrom.setAdapter(new PlacesAutoCompleteAdapter(this, R.layout.autocomplete_item));
         autoCompleteTo.setAdapter(new PlacesAutoCompleteAdapter(this, R.layout.autocomplete_item));
+
+		// small example of how to use the weather service
+		weatherService.getCurrentWeather(49.5778294, 10.9942273, new Callback<JsonNode>() {
+			@Override
+			public void success(JsonNode jsonNode, Response response) {
+				String weather = jsonNode.get("weather").get(0).get("main").asText();
+				Toast.makeText(
+						MainActivity.this,
+						"The weather in Erlangen is \"" + weather + "\" today",
+						Toast.LENGTH_LONG).show();
+			}
+
+			@Override
+			public void failure(RetrofitError error) {
+				Toast.makeText(
+						MainActivity.this,
+						"Failed to get weather: " + error.getMessage(),
+						Toast.LENGTH_LONG).show();
+			}
+		});
     }
 
 }
