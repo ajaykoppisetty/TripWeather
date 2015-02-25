9a7dc9e7cc342dc8dbc5a31b30a2d86a46256249
Simon Hofmann <mail@simon-hofmann.org>
2015-02-25 11:43:23 +0100

    directions: Added Builder to Route class

--- app/src/main/java/org/faudroids/tripweather/directions/Route.java
+++ app/src/main/java/org/faudroids/tripweather/directions/Route.java
@@ -4,44 +4,111 @@ import java.util.ArrayList;
 
 public class Route {
 
-    double length;
-    Waypoint origin = null;
-    Waypoint destination = null;
-    ArrayList<Waypoint> waypoints = new ArrayList<>();
 
-    public void setLength(double length) {
-        this.length = length;
+    public enum TravelMode {
+        walking(5), bicycling(15), driving(80);
+
+        private final int speed;
+
+        private TravelMode(int speed) {
+            this.speed = speed;
+        }
+
+        public int value() {
+            return speed;
+        }
     }
 
-    public void setOrigin(Waypoint origin) {
-        this.origin = origin;
+
+    private final Waypoint origin;
+    private final Waypoint destination;
+    private final String copyright;
+    private final String warnings;
+    private final TravelMode travelMode;
+    private final ArrayList<Waypoint> waypoints;
+
+
+    private Route(Builder builder) {
+        this.origin = builder.origin;
+        this.destination = builder.destination;
+        this.copyright = builder.copyright;
+        this.warnings = builder.warnings;
+        this.travelMode = builder.travelMode;
+        this.waypoints = builder.waypoints;
     }
 
-    public void setDestination(Waypoint destination) {
-        this.destination = destination;
-    }
-
-    public void setWaypoints(ArrayList<Waypoint> waypoints) {
-        this.waypoints = waypoints;
-    }
 
     public ArrayList<Waypoint> getWaypoints() {
         return waypoints;
     }
 
+
     public Waypoint getDestination() {
         return destination;
     }
+
 
     public Waypoint getOrigin() {
         return origin;
     }
 
-    public double getLength() {
-        return length;
+
+    public double getTotalLength() {
+        return 0;
     }
 
-    public ArrayList<Waypoint> getSegments(int segmentSize) {
-        return null;
+
+    public double getTotalTime() {
+        return 0;
+    }
+
+
+    public static class Builder {
+        private Waypoint origin;
+        private Waypoint destination;
+        private String copyright;
+        private String warnings;
+        private TravelMode travelMode;
+        private ArrayList<Waypoint> waypoints;
+
+        public Builder from(Waypoint origin) {
+            this.origin = origin;
+            return this;
+        }
+
+
+        public Builder to(Waypoint destination) {
+            this.destination = destination;
+            return this;
+        }
+
+
+        public Builder copyright(String message) {
+            this.copyright = message;
+            return this;
+        }
+
+
+        public Builder warnings(String message) {
+            this.warnings = message;
+            return this;
+        }
+
+
+        public Builder travelMode(TravelMode mode) {
+            this.travelMode = mode;
+            return this;
+        }
+
+
+        public Builder waypoints(ArrayList<Waypoint> wp) {
+            this.waypoints.addAll(wp);
+            return this;
+        }
+
+
+        public Route construct() {
+            return new Route(this);
+        }
     }
 }
