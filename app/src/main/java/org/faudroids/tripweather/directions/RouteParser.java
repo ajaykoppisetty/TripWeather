e17d0557a943abac0ee5f0514c60384968f2decc
Simon Hofmann <mail@simon-hofmann.org>
2015-02-25 12:12:17 +0100

    Updated RouteParser to make use of new Builder pattern

--- app/src/main/java/org/faudroids/tripweather/directions/RouteParser.java
+++ app/src/main/java/org/faudroids/tripweather/directions/RouteParser.java
@@ -4,8 +4,16 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class RouteParser {
+
+    private static HashMap<String, Route.TravelMode> travelModeHashMap = new HashMap<>();
+    static {
+        travelModeHashMap.put("DRIVING", Route.TravelMode.driving);
+        travelModeHashMap.put("WALKING", Route.TravelMode.walking);
+        travelModeHashMap.put("BICYCLING", Route.TravelMode.bicycling);
+    }
 
     private ObjectNode routeJSON = null;
 
@@ -24,54 +32,65 @@ public class RouteParser {
     }
 
     public ArrayList<Route> parse() {
-        ArrayList<Waypoint> wp = new ArrayList<>();
+        Route.Builder builder = new Route.Builder();
         ArrayList<Route> routeList = new ArrayList<>();
+
+        String copyright = null;
+        String warnings = null;
+        String travelMode = null;
+        Waypoint origin = null;
+        Waypoint destination = null;
+        ArrayList<Waypoint> wp = new ArrayList<>();
 
         JsonNode routes = routeJSON.get("routes");
 
         for(int routeIdx = 0; routeIdx < routes.size(); ++routeIdx) {
+            copyright = routes.get(routeIdx).get("copyrights").asText();
+            warnings = routes.get(routeIdx).get("warnings").asText();
+
             JsonNode legs = routes.get(routeIdx).get("legs");
 
-            if(legs != null) {
-                for (int legIdx = 0; legIdx < legs.size(); ++legIdx) {
-                    Route route = new Route();
-                    route.setOrigin(new Waypoint(legs.get(legIdx).get("start_location")
-                                                     .get("lat").asDouble(),
-                                                 legs.get(legIdx).get("start_location")
-                                                     .get("lng").asDouble(), 0, 0));
-
-                    route.setDestination(new Waypoint(legs.get(legIdx)
-                                                          .get("end_location")
-                                                          .get("lat").asDouble(),
-                                                      legs.get(legIdx)
-                                                          .get("end_location")
-                                                          .get("lng").asDouble(),
-                                                      legs.get(legIdx)
-                                                          .get("distance")
-                                                          .get("value").asDouble(),
-                                                      legs.get(legIdx).get("duration")
-                                                          .get("value").asDouble()));
-
-                    JsonNode steps = legs.get(legIdx).get("steps");
-
-                    for (int idx = 0; idx < steps.size(); ++idx) {
-                        wp.add(new Waypoint(steps.get(idx)
-                                                 .get("end_location")
-                                                 .get("lat").asDouble(),
-                                            steps.get(idx)
-                                                 .get("end_location")
-                                                 .get("lng").asDouble(),
-                                            steps.get(idx)
-                                                 .get("distance")
-                                                 .get("value").asDouble(),
-                                            steps.get(idx)
-                                                 .get("duration")
-                                                 .get("value").asDouble()));
-                    }
-
-                    route.setWaypoints(wp);
-                    routeList.add(route);
+            if(legs.size() > 0) {
+                if(legs.get(0).get("steps").size() > 0) {
+                    travelMode = legs.get(0).get("steps").get("travel_mode").asText();
                 }
+            }
+
+            for (int legIdx = 0; legIdx < legs.size(); ++legIdx) {
+                origin = new Waypoint(legs.get(legIdx).get("start_location")
+                                                 .get("lat").asDouble(),
+                                             legs.get(legIdx).get("start_location")
+                                                 .get("lng").asDouble());
+
+
+                destination = new Waypoint(legs.get(legIdx)
+                                                        .get("end_location")
+                                                        .get("lat").asDouble(),
+                                                    legs.get(legIdx)
+                                                        .get("end_location")
+                                                        .get("lng").asDouble());
+
+
+                JsonNode steps = legs.get(legIdx).get("steps");
+
+                for (int idx = 0; idx < steps.size(); ++idx) {
+                    wp.add(new Waypoint(steps.get(idx)
+                                             .get("end_location")
+                                             .get("lat").asDouble(),
+                                        steps.get(idx)
+                                             .get("end_location")
+                                             .get("lng").asDouble()));
+                }
+
+                Route route = new Route.Builder()
+                                       .from(origin)
+                                       .to(destination)
+                                       .travelMode (travelModeHashMap.get(travelMode))
+                                       .copyright(copyright)
+                                       .warnings(warnings)
+                                       .construct();
+                routeList.add(route);
+                wp.clear();
             }
         }
 
