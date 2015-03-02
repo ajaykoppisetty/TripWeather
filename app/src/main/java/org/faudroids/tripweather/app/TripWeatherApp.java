0c7af5eb59e6b5b2967dcea5fea72bff9e06a809
Philipp Eichhorn <philipp.eichhorn@bitdroid.de>
2015-03-02 20:44:26 +0100

    Added crashlytics support

--- app/src/main/java/org/faudroids/tripweather/app/TripWeatherApp.java
+++ app/src/main/java/org/faudroids/tripweather/app/TripWeatherApp.java
@@ -3,9 +3,13 @@ package org.faudroids.tripweather.app;
 
 import android.app.Application;
 
+import com.crashlytics.android.Crashlytics;
+
+import org.faudroids.tripweather.BuildConfig;
 import org.faudroids.tripweather.geo.GeoModule;
 import org.faudroids.tripweather.weather.WeatherModule;
 
+import io.fabric.sdk.android.Fabric;
 import roboguice.RoboGuice;
 import timber.log.Timber;
 
@@ -14,13 +18,35 @@ public final class TripWeatherApp extends Application {
 	@Override
 	public void onCreate() {
 		super.onCreate();
-		Timber.plant(new Timber.DebugTree());
+
 		RoboGuice.getOrCreateBaseApplicationInjector(
 				this,
 				RoboGuice.DEFAULT_STAGE,
 				RoboGuice.newDefaultRoboModule(this),
 				new WeatherModule(),
 				new GeoModule());
+
+		if (BuildConfig.DEBUG) {
+			Timber.plant(new Timber.DebugTree());
+		} else {
+			Fabric.with(this, new Crashlytics());
+			Timber.plant(new CrashReportingTree());
+		}
+	}
+
+
+	private static final class CrashReportingTree extends Timber.HollowTree {
+
+		@Override
+		public void e(String msg, Object... args) {
+			Crashlytics.log(msg);
+		}
+
+		@Override
+		public void e(Throwable e, String msg, Object... args) {
+			Crashlytics.log(msg);
+			Crashlytics.logException(e);
+		}
 	}
 
 }
